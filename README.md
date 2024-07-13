8d88498887ad83d83cfcd51b2187a3a820c9e60c
1connectable <175514164+1connecter@users.noreply.github.com>
2024-07-14 01:56:28 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# AutoRank - Bot
+- Bot fait par 1connectable
+- Dm 1connectable si vous trouvez un bug (ce qui devrait pas arriver)
---
+++ index.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+
+const intents = new Discord.IntentsBitField(3276799)
+
+const client = new Discord.Client({intents})
+
+const config = require('./config.json')
+
+client.once('ready', () => {
+
+    console.log(`${client.user.tag} est en ligne`);
+
+});
+
+client.on('messageCreate', (message) => {
+    
+    if (message.mentions.has(client.user)) {
+
+      if (message.channel.id !== config.channel) return;
+        
+      if (message.author.displayName && message.author.displayName.includes(config.tag)) {
+
+            const role = message.guild.roles.cache.get(config.role);
+
+            if (role) {
+
+                message.member.roles.add(role).then(() => {
+
+                        message.channel.send(`<@${message.author.id}>, vous avez maintenant le rôle ${role.name}!`);
+
+                    }).catch((error) => {
+
+                        console.log(`Erreur : ${error}`);
+
+                    });
+
+            } else {
+
+                message.channel.send("Erreur.");
+
+            }
+
+        } else {
+
+            message.channel.send(`<@${message.author.id}>, vous n'avez pas le tag '${config.tag}' dans votre pseudo. Veuillez le mettre.`);
+
+        }
+
+    }
+
+});
+
+client.login(config.token);
